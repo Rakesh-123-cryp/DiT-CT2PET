9c1ebdd35b3d7adebeec455b8f3c4a7b49f659ce
Rakesh-123-cryp <roketraj03@gmail.com>
2025-07-17 23:13:53 +0530

    Small bebug

--- train.py
+++ train.py
@@ -266,7 +266,7 @@ def main(args):
                 # Map input images to latent space + normalize latents:
                 x = vae.encode(x).latent_dist.sample().mul_(0.18215)
             t = torch.randint(0, diffusion.num_timesteps, (x.shape[0],), device=device)
-            model_kwargs = {'y': torch.zeros(x.shape[0]), dtype=torch.long, device=device}#dict(y=y)
+            model_kwargs = {'y': torch.zeros(x.shape[0], dtype=torch.long, device=device)}#dict(y=y)
             loss_dict = diffusion.training_losses(model, x, t, y, model_kwargs)
             loss = loss_dict["loss"].mean()
             opt.zero_grad()
