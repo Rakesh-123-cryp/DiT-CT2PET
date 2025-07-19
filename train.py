2705c271def6aa98115d8e691e0e02d43776e3b4
Rakesh-123-cryp <roketraj03@gmail.com>
2025-07-19 15:51:51 +0530

    Added code for loading from CKPT

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ class ImagePairDataset(Dataset):
                 transforms.Lambda(lambda pil_image: center_crop_arr(pil_image, image_size)),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                transforms.Normalize(mean=[0.5], std=[0.5], inplace=True)
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True)
             ])
         else:
             self.transform = transform
