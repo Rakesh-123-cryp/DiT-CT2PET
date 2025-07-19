2cad8dfd07065f5f45e688af89d28f4dd599a3f5
Rakesh-123-cryp <roketraj03@gmail.com>
2025-07-19 19:53:44 +0530

    small bugs

--- models.py
+++ models.py
@@ -325,6 +325,7 @@ class DiT(nn.Module):
         x = self.x_embedder(x) + self.pos_embed  # (N, T, D), where T = H * W / patch_size ** 2
         t = self.t_embedder(t)                   # (N, D)
         condition = self.x_embedder(y) + self.pos_embed_cond     # y = self.y_embedder(y, self.training)    # (N, D)
+        print(condition.shape)
         c = t + condition                          # (N, D)
         for block in self.blocks:
             x = block(x, c)                        # (N, T, D)
